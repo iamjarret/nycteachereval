c116a12402e82447b3af5638b33fb5b625a52198
iamjarret <iamjarret@gmail.com>
2013-03-04 16:40:50 -0500

    fixed static serve issue

--- nycteacher/urls.py
+++ nycteacher/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from teachers import views
-
-# Uncomment the next two lines to enable the admin:
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
@@ -23,3 +22,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ other/socarata_api.py
@@ -0,0 +1,24 @@
+#import 
+
+TOKEN = "NYwwNBzQKhL97QPfsnlWh92H61bucH3a7BIw"
+ENDPOINT = "http://data.cityofnewyork.us/resource/4s7y-vm5x.json"
+
+'''
+GRADE LEVEL
+    grade_level
+PROGRAM TYPE
+    program_type
+CORE SUBJECT
+    core_subject
+SERVICE CATEGORY
+    service_category
+CLASS SIZE
+    class_size
+NUMBER OF CLASSES
+    number_of_classes
+NUMBER OF STUDENTS
+    number_of_students
+PERCENT OF STUDENTS IN PROGRAM / GRADE / SUBJECT
+    percent_of_students_in_program_grade_subject
+
+'''
